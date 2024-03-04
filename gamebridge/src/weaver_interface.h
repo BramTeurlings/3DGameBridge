3d9b8e04a929279b263d54d90122537d85ef52a3
Joey <jvanhoek@hotmail.com>
2024-03-04 17:24:39 +0100

    Made the platform mgr and dx12 weavetr simpler for now

---
+++ gamebridge/src/weaver_interface.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <sr/management/srcontext.h>
+#include "game_bridge_manager_interface.h"
+
+class GameBridgeWeaver : public IGameBridgeManager
+{
+public:
+    GameBridgeWeaver() = default;
+    virtual void InitializeWeaver(SR::SRContext sr_context) = 0;
+};
